bd326cb10f8fa0a4b765119969bc082d716e625b
Éloi Rivard <eloi@yaal.coop>
2024-11-22 11:43:32 +0100

    tests: add a test about content types handling

--- tests/test_network.py
+++ tests/test_network.py
@@ -2,12 +2,14 @@ import re
 
 from httpx import Client
 from scim2_client import SCIMClient
+from scim2_models import Context
 from scim2_models import Error
 from scim2_models import Group
 from scim2_models import User
 
 from scim2_tester.checker import check_schemas_endpoint
 from scim2_tester.checker import check_server
+from scim2_tester.resource import check_object_query
 from scim2_tester.utils import Status
 
 
@@ -40,3 +42,45 @@ def test_bad_authentication(httpserver):
         result.reason
         == "The server returned a SCIM Error object: Authentication is needed"
     )
+
+
+def test_bad_content_type(httpserver):
+    """Test reaching a valid URL returning an invalid content type."""
+    scim_user = User(id="scim", username="scim")
+    json_user = User(id="json", username="json")
+    invalid_user = User(id="invalid", username="invalid")
+    missing_user = User(id="missing", username="missing")
+
+    httpserver.expect_request(re.compile(r"/Users/scim")).respond_with_json(
+        scim_user.model_dump(scim_ctx=Context.RESOURCE_QUERY_RESPONSE),
+        content_type="application/scim+json",
+    )
+    httpserver.expect_request(re.compile(r"/Users/json")).respond_with_json(
+        json_user.model_dump(scim_ctx=Context.RESOURCE_QUERY_RESPONSE),
+        content_type="application/json",
+    )
+    httpserver.expect_request(re.compile(r"/Users/invalid")).respond_with_json(
+        invalid_user.model_dump(scim_ctx=Context.RESOURCE_QUERY_RESPONSE),
+        content_type="application/invalid",
+    )
+    httpserver.expect_request(re.compile(r"/Users/missing")).respond_with_json(
+        invalid_user.model_dump(scim_ctx=Context.RESOURCE_QUERY_RESPONSE),
+        content_type="",
+    )
+
+    client = Client(base_url=f"http://localhost:{httpserver.port}")
+    scim = SCIMClient(client, resource_types=(User, Group))
+
+    result = check_object_query(scim, scim_user)
+    assert result.status == Status.SUCCESS
+
+    result = check_object_query(scim, json_user)
+    assert result.status == Status.SUCCESS
+
+    result = check_object_query(scim, invalid_user)
+    assert result.status == Status.ERROR
+    assert result.reason == "Unexpected content type: application/invalid"
+
+    result = check_object_query(scim, missing_user)
+    assert result.status == Status.ERROR
+    assert result.reason == "Unexpected content type: "
